7ab3500ec40fbff95574cd089faba6e68d65a3b6
venu <o190679@rguktong.ac.in>
2024-02-13 12:11:28 +0530

    added signIn functionality and storing the user data in database

--- src/misc/firebase.jsx
+++ src/misc/firebase.jsx
@@ -1,15 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
 const firebaseConfig = {
-  apiKey: "AIzaSyBT9rE6MVCzfwzUhQtBq4TbfuK6yJisa-M",
-  authDomain: "vchat-ce52a.firebaseapp.com",
-  databaseURL: "https://vchat-ce52a-default-rtdb.firebaseio.com",
-  projectId: "vchat-ce52a",
-  storageBucket: "vchat-ce52a.appspot.com",
-  messagingSenderId: "726826815268",
-  appId: "1:726826815268:web:0a03b4462dde6e88506db1"
+  apiKey: 'AIzaSyBT9rE6MVCzfwzUhQtBq4TbfuK6yJisa-M',
+  authDomain: 'vchat-ce52a.firebaseapp.com',
+  databaseURL: 'https://vchat-ce52a-default-rtdb.firebaseio.com',
+  projectId: 'vchat-ce52a',
+  storageBucket: 'vchat-ce52a.appspot.com',
+  messagingSenderId: '726826815268',
+  appId: '1:726826815268:web:0a03b4462dde6e88506db1',
 };
-
 const app = initializeApp(firebaseConfig);
-export const auth=app.getAuth();
+export const auth = getAuth(app);
+export const database = getDatabase(app);
--- src/pages/signIn.jsx
+++ src/pages/signIn.jsx
@@ -1,45 +1,64 @@
-import React from 'react'
-import { Button, Col, Container ,Grid, Icon, Panel, Row} from 'rsuite';
-import { auth } from '../misc/firebase';
-import {signInWithPopup,GoogleAuthProvider}  from "firebase/auth";
-const SignIn = () => {
-    const singInwithProvide= async (provider)=>{
-        try{
-        const result =await signInWithPopup(auth,provider);
-        console.log(result);
-        }
-        catch(error){
-            console.log(error.message);
+import React from 'react';
+import { Alert, Button, Col, Container, Grid, Icon, Panel, Row } from 'rsuite';
+import { auth, database } from '../misc/firebase';
+import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { serverTimestamp, ref, set } from 'firebase/database';
 
-        }
+const SignIn = () => {
+  const writeUserData = async (userId, name, email, imageUrl) => {
+    try {
+      const info = await set(ref(database, `/profiles/${userId}`), {
+        username: name,
+        email: email,
+        profile_picture: imageUrl,
+        created_at: serverTimestamp(),
+      });
+      console.log(info);
+      Alert.info('Written', 2000);
+    } catch (error) {
+      Alert.warning(error.message, 2000);
     }
-    const signInwithGoogle=()=>{
-        singInwithProvide(new GoogleAuthProvider());
+  };
+  const singInwithProvide = async provider => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      console.log(result);
+      writeUserData(
+        result.user.uid,
+        result.user.displayName,
+        result.user.email,
+        result.user.photoURL
+      );
+    } catch (error) {
+      Alert.error(error.message);
+      console.log(error.message);
     }
+  };
+  const signInwithGoogle = () => {
+    singInwithProvide(new GoogleAuthProvider());
+  };
 
   return (
     <Container>
-        <Grid className='mt-page'>
-            <Row>
-                <Col xs={24} md={12} mdOffset={6} className='text-center'>
-                    <Panel>
-                        <div>
-                            <h2>Let's Chat</h2>
-                            <p>A simple chatting application</p>
-                            <div className="mt-3">
-                                <Button block color='orange' onClick={signInwithGoogle} >
-                                    <Icon icon={"google"} /> SignIn with Google
-                                </Button>
-                            </div>
-                        </div>
-                        
-                    </Panel>
-                </Col>
-            </Row>
-
-        </Grid>
+      <Grid className="mt-page">
+        <Row>
+          <Col xs={24} md={12} mdOffset={6} className="text-center">
+            <Panel>
+              <div>
+                <h2>Let's Chat</h2>
+                <p>A simple chatting application</p>
+                <div className="mt-3">
+                  <Button block color="orange" onClick={signInwithGoogle}>
+                    <Icon icon={'google'} /> SignIn with Google
+                  </Button>
+                </div>
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
     </Container>
-  )
-}
+  );
+};
 
 export default SignIn;
