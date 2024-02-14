19c5ce1d9a08267ee3bd5420ccf6114947660dd7
venu naik <yedukondalunaik12345@gmail.com>
2024-02-14 22:35:18 +0530

    Avatar editor setup and uploading has to be done

--- src/components/dashboard/UploadAvatarBtn.jsx
+++ src/components/dashboard/UploadAvatarBtn.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useModalState } from '../../misc/customhook';
 import { Alert, Button, Icon, Modal } from 'rsuite';
-
+import AvatarEditor from 'react-avatar-editor';
 const UploadAvatarBtn = () => {
   const { isOpen, open, close } = useModalState();
   const [image, setImage] = useState(null);
@@ -14,9 +14,9 @@ const UploadAvatarBtn = () => {
     const currentFile = e.target.files;
     if (currentFile.length === 1) {
       const file = currentFile[0];
-      console.log(file);
+
       if (validFile(file.type)) {
-        setImage(image);
+        setImage(file);
         open();
       } else {
         Alert.warning(`Wrong file type ${file.type}`, 3000);
@@ -26,10 +26,10 @@ const UploadAvatarBtn = () => {
   return (
     <div className="mt-8 ">
       <label htmlFor="avatar-upload" className=" relative cursor-pointer ">
-        <h3>Select new avatar</h3>
+        Select new avatar
         <input
           type="file"
-          className=" d-none"
+          className="cursor-pointer"
           accept={fileInputTypes}
           onChange={handleFileChange}
         />
@@ -37,11 +37,24 @@ const UploadAvatarBtn = () => {
 
       <Modal show={isOpen} onHide={close}>
         <Modal.Header>
-          <Modal.Title> Upload new Avatar</Modal.Title>
+          <Modal.Title className="text-center">Upload new Avatar</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Image will appear here</Modal.Body>
-        <Modal.Footer>
-          <Button color="red" appearance="ghost">
+        <Modal.Body>
+          {image && (
+            <div className="d-flex justify-content-center align-center">
+              <AvatarEditor
+                image={image}
+                width={200}
+                height={200}
+                border={10}
+                scale={1}
+                borderRadius={100}
+              />
+            </div>
+          )}
+        </Modal.Body>
+        <Modal.Footer className="d-flex justify-content-center">
+          <Button color="red" >
             <Icon icon={'upload'} />
             Upload Avatar
           </Button>
