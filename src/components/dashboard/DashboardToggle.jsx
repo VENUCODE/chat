62a19d1de1acbc4598e4636d523074611ddf834b
venu naik <yedukondalunaik12345@gmail.com>
2024-02-14 12:13:58 +0530

    added responsise drawer

--- src/components/dashboard/DashboardToggle.jsx
+++ src/components/dashboard/DashboardToggle.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Button, Drawer, Icon } from 'rsuite';
-import useModalState from '../../misc/customhook';
+import { useModalState, useMediaQuery } from '../../misc/customhook';
 import Dashboard from '.';
 
 const DashboardToggle = () => {
   const { isOpen, open, close } = useModalState();
+  const isMobile = useMediaQuery('(max-width:992px)');
   return (
     <>
       <Button color="blue" block onClick={open}>
         <Icon icon="dashboard" /> Dashboard
       </Button>
-      <Drawer show={isOpen} onHide={close} placement="left">
+      <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
         <Dashboard />
       </Drawer>
     </>
--- src/misc/customhook.js
+++ src/misc/customhook.js
@@ -1,9 +1,28 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
-function useModalState(defaultValue = false) {
+export function useModalState(defaultValue = false) {
   const [isOpen, setisOpen] = useState(false);
   const open = useCallback(() => setisOpen(true), []);
   const close = useCallback(() => setisOpen(false), []);
   return { isOpen, open, close };
 }
-export default useModalState;
+// usage
+// const is992px = useMediaQuery('(max-width: 992px)')
+
+export const useMediaQuery = query => {
+  const [matches, setMatches] = useState(
+    () => window.matchMedia(query).matches
+  );
+
+  useEffect(() => {
+    const queryList = window.matchMedia(query);
+    setMatches(queryList.matches);
+
+    const listener = evt => setMatches(evt.matches);
+
+    queryList.addListener(listener);
+    return () => queryList.removeListener(listener);
+  }, [query]);
+
+  return matches;
+};
